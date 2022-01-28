6d43a9b5c0eb468e14b78b34072e1be1a9a7e8f0
Jakub Onderka <jakub.onderka@gmail.com>
2022-01-28 23:55:47 +0100

    fixup! Code style according to PSR12

--- src/OpenIDConnectClient.php
+++ src/OpenIDConnectClient.php
@@ -1,5 +1,8 @@
 <?php
 
+// phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
+// phpcs:disable PSR12.Properties.ConstantVisibility.NotFound
+// phpcs:disable Generic.Files.LineLength.TooLong
 /**
  * Copyright MITRE 2020
  * Copyright Jakub Onderka 2022
@@ -154,7 +157,7 @@ class Jwks
             }
         }
         if (isset($header->kid)) {
-            throw new OpenIDConnectClientException("Unable to find a key for {$header->alg} with kid `{$header->kid}`");
+            throw new OpenIDConnectClientException("Unable to find a key for $header->alg with kid `$header->kid`");
         }
         throw new OpenIDConnectClientException("Unable to find a key for $keyType");
     }
@@ -729,7 +732,7 @@ class OpenIDConnectClient
 
         $response = $this->fetchURL($wellKnownConfigUrl);
         if ($response->responseCode !== 200) {
-            throw new OpenIDConnectClientException("Invalid response code {$response->responseCode} when fetching wellKnow, expected 200");
+            throw new OpenIDConnectClientException("Invalid response code $response->responseCode when fetching wellKnow, expected 200");
         }
         $wellKnown = $this->jsonDecode($response->data);
 
@@ -771,7 +774,7 @@ class OpenIDConnectClient
             return $default;
         }
 
-        throw new OpenIDConnectClientException("The provider {$param} could not be fetched. Make sure your provider has a well known configuration available.");
+        throw new OpenIDConnectClientException("The provider $param could not be fetched. Make sure your provider has a well known configuration available.");
     }
 
     /**
@@ -1034,7 +1037,7 @@ class OpenIDConnectClient
 
         if ($this->tokenAuthenticationMethod && !in_array($this->tokenAuthenticationMethod, $authMethodsSupported)) {
             $supportedMethods = implode(", ", $authMethodsSupported);
-            throw new OpenIDConnectClientException("Token authentication method {$this->tokenAuthenticationMethod} is not supported by IdP. Supported methods are: $supportedMethods");
+            throw new OpenIDConnectClientException("Token authentication method $this->tokenAuthenticationMethod is not supported by IdP. Supported methods are: $supportedMethods");
         }
 
         $headers = [];
@@ -1344,7 +1347,8 @@ class OpenIDConnectClient
      * given_name       string      Given name or first name of the End-User.
      * family_name      string      Surname or last name of the End-User.
      * middle_name      string      Middle name of the End-User.
-     * nickname         string      Casual name of the End-User that may or may not be the same as the given_name. For instance, a nickname value of Mike might be returned alongside a given_name value of Michael.
+     * nickname         string      Casual name of the End-User that may or may not be the same as the given_name.
+     *                              For instance, a nickname value of Mike might be returned alongside a given_name value of Michael.
      * profile          string      URL of End-User's profile page.
      * picture          string      URL of the End-User's profile picture.
      * website          string      URL of End-User's web page or blog.
@@ -1355,7 +1359,8 @@ class OpenIDConnectClient
      * zoneinfo         string      String from zoneinfo [zoneinfo] time zone database. For example, Europe/Paris or America/Los_Angeles.
      * locale           string      The End-User's locale, represented as a BCP47 [RFC5646] language tag.
      *                              This is typically an ISO 639-1 Alpha-2 [ISO639‑1] language code in lowercase and an ISO 3166-1 Alpha-2 [ISO3166‑1] country code in uppercase, separated by a dash.
-     *                              For example, en-US or fr-CA. As a compatibility note, some implementations have used an underscore as the separator rather than a dash, for example, en_US; Implementations MAY choose to accept this locale syntax as well.
+     *                              For example, en-US or fr-CA. As a compatibility note, some implementations have used an underscore as the separator rather than a dash, for example, en_US;
+     *                              Implementations MAY choose to accept this locale syntax as well.
      * phone_number     string      The End-User's preferred telephone number. E.164 [E.164] is RECOMMENDED as the format of this Claim. For example, +1 (425) 555-1212 or +56 (2) 687 2400.
      * address          JSON object The End-User's preferred address. The value of the address member is a JSON [RFC4627] structure containing some or all of the members defined in Section 2.4.2.1.
      * updated_time     string      Time the End-User's information was last updated, represented as a RFC 3339 [RFC3339] datetime. For example, 2011-01-03T23:58:42+0000.
